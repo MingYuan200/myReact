c1f27508f07d7e226c1da2f21a73e778746a1185
MingYuan200 <yuan40133@gmail.com>
2024-11-19 08:50:11 +0800

    1119

---
+++ my-react-todo/todo-demo/src/components/Todo.jsx
@@ -0,0 +1,12 @@
+// 解構props的屬性名，用{屬姓名}包起來
+function Todo({todo}) {
+    return (
+        <div className="todo">
+            {/* <p>todo</p> */}
+            {/* <p>{todo}</p> */}
+            <p>{todo.content}</p>
+        </div>
+    )
+}
+
+export default Todo
---
+++ my-react-todo/todo-demo/src/components/TodoWrapper.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react"
+import CreateForm from "./CreateForm"
+import Todo from "./Todo"
+
+function TodoWrapper() {
+    // 因為有N個todo，所以使用陣列存取
+    // const [todos, setTodos] = useState(['list1', 'list2']);
+    // 因為陣列需要key屬性，所以要改成陣列物件。加上id
+    const [todos, setTodos] = useState([
+        {content:'List1', id:Math.random()},
+        {content:'List2', id:Math.random()},
+    ]);
+
+    // 建立加入新的todo內容
+    // 1.使用...其餘運篹子，來保留原陣列內容
+    // 2.再加入新的物件內容
+    const addTodo = (content)=>{
+        setTodos([...todos, {content:content, id:Math.random()}])
+    }
+
+    return (
+        <div className="wrapper">
+            <h1>待辦事項</h1>
+            <CreateForm addTodo={addTodo} />
+            {
+                todos.map((todo)=>{
+                    return <Todo todo={todo} key={todo.id} />
+                })
+            }
+            
+        </div>
+    )
+}
+export default TodoWrapper
