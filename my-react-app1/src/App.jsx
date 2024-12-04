f35cf685b1357e546378ccf3e53c627ff0cd42c1
MingYuan200 <yuan40133@gmail.com>
2024-12-04 14:58:14 +0800

    1204

--- my-react-app1/src/App.jsx
+++ my-react-app1/src/App.jsx
@@ -1,115 +1,19 @@
-import { useEffect } from "react"
-import axios from "axios";
-import "./App.css";
-import { IoUmbrella } from "react-icons/io5";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./component/Navbar";
+import About from "./pages/About";
+import Home from "./pages/Home";
 
 
-function App() {
-
-  //非同步讀取json資料
-  useEffect(() => {
-    (async () => {
-      // const data = await axios.get('./F-C0032-001.json');
-      const data = await axios.get('https://github.com/MingYuan200/myReact/blob/main/my-react-app1/src/F-C0032-001.json');
-      const { location } = data.data.cwaopendata.dataset;
-      console.log(location);
-    })
-  }, [])
-
-
+export default function App() {
   return (
-    <>
-      <div className="wrap">
+    <div className="wrap">
+      <Navbar />
 
-        <h2>36小時天氣預報</h2>
-
-
-        <div className="row">
-          <div className="col">
-            <div className="card">
-              <div className="card-title">台北市</div>
-              <div className="card-body">
-                <div className="row2">
-
-                <div className="col2">
-                <p className="p1">2日</p>
-                <p>上午6:00<br />~<br />下午6:00</p>
-                <figure>
-                  <img src="../weathericon/多雲.svg" alt="" /></figure>
-                <p>晴時多雲</p>
-                <p><IoUmbrella />10%</p>
-                </div>
-
-                <div className="col2">
-                <p className="p1">2日</p>
-                <p>上午6:00<br />~<br />下午6:00</p>
-                <figure>
-                  <img src="../weathericon/多雲.svg" alt="" /></figure>
-                <p>晴時多雲</p>
-                <p><IoUmbrella />10%</p>
-                </div>
-
-                <div className="col2">
-                <p className="p1">2日</p>
-                <p>上午6:00<br />~<br />下午6:00</p>
-                <figure>
-                  <img src="../weathericon/多雲.svg" alt="" /></figure>
-                <p>晴時多雲</p>
-                <p><IoUmbrella />10%</p>
-                </div>
-
-                </div>
-              </div>
-
-
-            </div>
-          </div>
-
-          <div className="col">
-            <div className="card">
-              <div className="card-title">新北市</div>
-              <div className="card-body">
-                <div className="row2">
-
-                <div className="col2">
-                <p className="p1">2日</p>
-                <p>上午6:00<br />~<br />下午6:00</p>
-                <figure>
-                  <img src="../weathericon/多雲.svg" alt="" /></figure>
-                <p>晴時多雲</p>
-                <p><IoUmbrella />10%</p>
-                </div>
-
-                <div className="col2">
-                <p className="p1">2日</p>
-                <p>上午6:00<br />~<br />下午6:00</p>
-                <figure>
-                  <img src="../weathericon/多雲.svg" alt="" /></figure>
-                <p>晴時多雲</p>
-                <p><IoUmbrella />10%</p>
-                </div>
-
-                <div className="col2">
-                <p className="p1">2日</p>
-                <p>上午6:00<br />~<br />下午6:00</p>
-                <figure>
-                  <img src="../weathericon/多雲.svg" alt="" /></figure>
-                <p>晴時多雲</p>
-                <p><IoUmbrella />10%</p>
-                </div>
-
-                </div>
-              </div>
-
-
-            </div>
-          </div>
-
-        </div>
-
-      </div>
-
-    </>
+      <hr />
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/About" element={<About/>}></Route>
+      </Routes>
+    </div>
   )
 }
-export default App
